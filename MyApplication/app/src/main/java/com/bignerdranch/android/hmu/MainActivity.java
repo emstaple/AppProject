efb33c874858d892f1c857420c84de4a041cf675
emstaple <emstaple@gmail.com>
2015-06-30 12:32:16 -0700

    Added Facebook Login
    Added Facebook login button and a basic log in screen.

--- MyApplication/app/src/main/java/com/bignerdranch/android/hmu/MainActivity.java
+++ MyApplication/app/src/main/java/com/bignerdranch/android/hmu/MainActivity.java
@@ -1,20 +1,16 @@
 package com.bignerdranch.android.hmu;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.facebook.FacebookSdk;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
     }
 
     @Override
